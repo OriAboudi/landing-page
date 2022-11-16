c919cb6878a0a207c805ff76b63e72896de49fd4
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-11-16 10:10:08 +0200

    ok

--- src/components/mainContent/secion2.jsx
+++ src/components/mainContent/secion2.jsx
@@ -4,7 +4,7 @@ function Secion2() {
   return (
     <div className='container-fluid '>
       <div className="container d-flex main-secion2">
-        <div className=" d-flex f ">
+        <div className=" d-flex main-secion2 ">
           <div className='secion2 col-md-6 m-auto'>
             <h2>Connect with customers everywhere</h2>
             <p className='p1'>Sell online, in person, or both with the marketing tools, social integrations, and sales channels you need to get your products in front of customers—and out the door.</p>
--- src/components/mainContent/secion3.jsx
+++ src/components/mainContent/secion3.jsx
@@ -4,10 +4,10 @@ function Secion3() {
   return (
     <div className='container-fluid my-4'>
       <div className="container">
-        <div className='d-flex'>
+        <div className='d-flex main-secion3'>
           <div className='img-secion3 col-md-6 m-auto'></div>
 
-          <div className='col-md-6 m-auto secion3'>
+          <div className='col-md-6  secion3'>
             <h2 className='h2-secion'>Everything you need to succeed</h2>
             <p className='p1'>Manage inventory, track payments, and view real-time business insights from a single dashboard. We’ve built all the tools you need, so you can focus on building your business.</p>
             <p className='p2'>Do more from day one</p>
