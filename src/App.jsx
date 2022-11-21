9f0a622dc2bbdba0d30a6a5191abc2959061c039
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-11-21 18:02:45 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 
 import './App.css';
-import Cars from './components/cars/cars';
 import Crypto from './components/crypto/crypto';
 
 
 function App() {
   return (
     <div className="App">
-
-      {/* <Cars/> */}
       <Crypto/>
     </div>
   );
