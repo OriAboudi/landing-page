18276ae6c596a983065b03d815724e2293ca8ac2
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-11-28 11:08:24 +0200

    ok

--- src/components/cars/item.jsx
+++ src/components/cars/item.jsx
@@ -5,14 +5,14 @@ function Item({ item }) {
     return (
 
         <div className='col-sm-6 col-md-3 shadow p-0 m-4 mb-5 border-1 border'>
-            <div className=' p-3'>
+            <div className='p-3 '>
                 <p className='text-center display-6 mb-4' >{item.company}</p>
                 <p >Model: {item.model}</p>
                 <p >Price: {item.price}</p>
                 <p >Year: {item.year}</p>
                 <p >Category: {item.category}</p>
             </div>
-            <div> <img  className='w-100 ' src={item.img_url} alt={item.model} /></div>
+            <div className='mb-0'> <img  className='w-100 mb-0  ' width={100} src={item.img_url} alt={item.model} /></div>
         </div>
 
 
