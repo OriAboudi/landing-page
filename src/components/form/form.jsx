40ff7e37a0b6f429bf08cd66b6c1449c64fe4c3c
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-11-28 15:42:01 +0200

    ok

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -41,7 +41,7 @@ const Form = () => {
     const emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
     const onSub = (_bodyData) => {
-        console.log(_bodyData);
+        console.log(_bodyData)
     }
 
     return (
