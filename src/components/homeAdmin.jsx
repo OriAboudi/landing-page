a24c4b1fdf10bdf0d8157edd947e2e427df4ee28
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-12-12 11:33:15 +0200

    ok

---
+++ src/components/homeAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeAdmin() {
+  return (
+    <div>HomeAdmin</div>
+  )
+}
+
+export default HomeAdmin
--- src/routers/appRouters.jsx
+++ src/routers/appRouters.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../components/home'
+import HomeAdmin from '../components/homeAdmin'
 import Page1 from '../components/page1'
 import Page2 from '../components/page2'
 import Page3 from '../components/page3'
-import Layout from '../layout/layout'
+import LayoutAdmin from '../layout/layoutAdmin/layoutAdmin'
+
+
+import Layout from '../layout/layoutUser/layout'
+
 function AppRouters() {
   return (
-<Router>
-    <Routes>
-        <Route path='/' element={<Layout/>}>
-            <Route index element={<Home/>}/>
-            <Route path='/page1' element={<Page1/>}/>
-            <Route path='/page2' element={<Page2/>}/>
-            <Route path='/page3' element={<Page3/>}/>
-            <Route path='/*' element={<h1>Page Not Found!</h1>}/>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/page1' element={<Page1 />} />
+          <Route path='/page2' element={<Page2 />} />
+          <Route path='/page3' element={<Page3 />} />
         </Route>
-    </Routes>
-</Router>
+
+        <Route path='/admin' element={<LayoutAdmin />}>
+          <Route path='/admin' element={<HomeAdmin />} />
+        </Route>
+
+        <Route path='/*' element={<h1>Page Not Found!</h1>} />
+      </Routes>
+    </Router>
   )
 }
 
