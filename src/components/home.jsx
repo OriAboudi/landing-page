a47dc6fbcbf0cd282e03a45bea5ca9301fa97fac
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-12-19 21:23:07 +0200

    ok

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,42 @@
+import axios from 'axios'
 import React from 'react'
 
+import { useState, useEffect } from 'react'
+
 function Home() {
+  const [data, setData] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  const doApi = async () => {
+    setLoading(true)
+    let url = 'https://www.omdbapi.com/?s=bank&y=2020&apiKey=d592be1f'
+    const { data } = await axios.get(url);
+    console.log(data.Search);
+    setData(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    doApi();
+  }, [])
+
+
+
   return (
-    <div>Home</div>
+    <div>
+      {loading ? <p>Loading...</p> :
+        data?.map((item) => {
+          return (
+            <div>
+              <p>{item.Search.Title}</p>
+              <p>{item.Search.Year}</p>
+              <p>{item.Search.Type}</p>
+              <p>{item.Search.Poster}</p>
+            </div>
+          )
+
+        })}
+    </div>
   )
 }
 
