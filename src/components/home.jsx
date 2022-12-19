0482891fba4efaab22fbbb58f5d5793337daba1e
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-12-19 22:08:30 +0200

    ok

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,40 +2,50 @@ import axios from 'axios'
 import React from 'react'
 
 import { useState, useEffect } from 'react'
+import Page1 from './page1'
 
 function Home() {
   const [data, setData] = useState({})
+
   const [loading, setLoading] = useState(false)
 
   const doApi = async () => {
     setLoading(true)
     let url = 'https://www.omdbapi.com/?s=bank&y=2020&apiKey=d592be1f'
     const { data } = await axios.get(url);
-    console.log(data.Search);
+    console.log(data);
+
+    // let obj = {
+    //   Poster: data.Search[0].Poster,
+    //   Title: data.Search.Title,
+    //   Type: data.Search.Type,
+    //   Year: data.Search.Year,
+    //   imdbID: data.Search.ImdbID
+    // }
+
+
     setData(data);
     setLoading(false);
   }
+  console.log(data);
 
   useEffect(() => {
     doApi();
   }, [])
 
 
-
+  console.log(data);
+  {/* {loading ? <p>Loading...</p> : */ }
   return (
     <div>
-      {loading ? <p>Loading...</p> :
-        data?.map((item) => {
-          return (
-            <div>
-              <p>{item.Search.Title}</p>
-              <p>{item.Search.Year}</p>
-              <p>{item.Search.Type}</p>
-              <p>{item.Search.Poster}</p>
-            </div>
-          )
+      <div>
+        { data?.map((item, i) =>
 
-        })}
+          <Page1 key={i} item={item} />
+
+
+        )}
+      </div>
     </div>
   )
 }
