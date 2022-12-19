d4f75f514ef2c1dd1c134863bf932c081bdad09a
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-12-19 22:32:46 +0200

    ok

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,46 +1,40 @@
 import axios from 'axios'
-import React from 'react'
 
-import { useState, useEffect } from 'react'
+
+import { useState, useEffect, useRef } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import Page1 from './page1'
 
 function Home() {
   const [data, setData] = useState({})
-
+  const inputRef = useRef()
   const [loading, setLoading] = useState(false)
-
+  const nav = useNavigate()
+  const param = useParams()
   const doApi = async () => {
     setLoading(true)
-    let url = 'https://www.omdbapi.com/?s=bank&y=2020&apiKey=d592be1f'
+    let url = `https://www.omdbapi.com/?s=${param}||bank&y=2020&apiKey=d592be1f`
     const { data } = await axios.get(url);
     console.log(data);
+    setData(data.Search);
+    // setLoading(false);
 
-    // let obj = {
-    //   Poster: data.Search[0].Poster,
-    //   Title: data.Search.Title,
-    //   Type: data.Search.Type,
-    //   Year: data.Search.Year,
-    //   imdbID: data.Search.ImdbID
-    // }
-
-
-    setData(data);
-    setLoading(false);
   }
-  console.log(data);
-
+  console.log(inputRef);
   useEffect(() => {
     doApi();
   }, [])
 
 
-  console.log(data);
-  console.log('1111');
-  {/* {loading ? <p>Loading...</p> : */ }
+
+
   return (
     <div>
+      <input placeholder='Search' onInput={() => {
+        nav.get(inputRef.current.value)
+      }} ref={inputRef} type="text" />
       <div>
-        { data?.map((item, i) =>
+        {loading ? <p>Loading...</p> : data.map((item, i) =>
 
           <Page1 key={i} item={item} />
 
