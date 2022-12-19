9e49948396a56ee3452902766e5e4f220150c698
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-12-19 22:40:01 +0200

    ok

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,14 +11,20 @@ function Home() {
   const [loading, setLoading] = useState(false)
   const nav = useNavigate()
   const param = useParams()
-  const doApi = async () => {
-    setLoading(true)
-    let url = `https://www.omdbapi.com/?s=${param}||bank&y=2020&apiKey=d592be1f`
-    const { data } = await axios.get(url);
-    console.log(data);
-    setData(data.Search);
-    // setLoading(false);
 
+  const doApi = async () => {
+    try {
+
+      setLoading(true)
+      let url = `https://www.omdbapi.com/?s=${param}||bank&y=2020&apiKey=d592be1f`
+      const { data } = await axios.get(url);
+      console.log(data);
+      setData(data.Search);
+      // setLoading(false);
+
+    } catch (error) {
+      console.log(error);
+    }
   }
   console.log(inputRef);
   useEffect(() => {
@@ -30,17 +36,22 @@ function Home() {
 
   return (
     <div>
-      <input placeholder='Search' onInput={() => {
-        nav.get(inputRef.current.value)
+      <input placeholder='Search' onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          nav.get(inputRef.current.value)
+        }
+      }} onInput={() => {
+
+
       }} ref={inputRef} type="text" />
-      <div>
+      {/* <div>
         {loading ? <p>Loading...</p> : data.map((item, i) =>
 
           <Page1 key={i} item={item} />
 
 
         )}
-      </div>
+      </div> */}
     </div>
   )
 }
