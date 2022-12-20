b6674dd5bc3917b365488850c57c5e0e04a1529d
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-12-20 11:14:26 +0200

    ok

--- src/components/page2.jsx
+++ src/components/page2.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 
 function Page2() {
 
   const param = useParams()
   console.log(param.id);
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  const doApiInfo = ()=>{
-    let url ='';
-
-
+  const doApiInfo = async () => {
+    setLoading(true)
+    let url = `http://www.omdbapi.com/?i=${param.id}&apiKey=d592be1f`;
+    const { data } = await axios.get(url)
+    console.log(data);
+    setData(data)
+    setLoading(false)
   }
+
+  useEffect(() => {
+    doApiInfo()
+  }, [])
   return (
     <div>
 
