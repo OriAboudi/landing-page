3191cfc7f96a5ad267fe95be5314b6eab0bfb30b
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2022-12-20 16:31:21 +0200

    ok

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,16 +8,18 @@ import axios from 'axios'
 
 function Home() {
   const [data, setData] = useState([])
-  const inputRef = useRef()
   const [loading, setLoading] = useState(false)
-  const [year, setYear] = useState('2022')
+  const [year, setYear] = useState(new Date().getFullYear())
+  const [timeline, setTimeline] = useState([])
+
+  const inputRef = useRef()
   const nav = useNavigate()
   const param = useParams()
   console.log(param.search);
   const doApi = async () => {
     try {
-
       setLoading(true)
+
       let url = `https://www.omdbapi.com/?s=${param.search || "bank"}&y=${year}&apiKey=d592be1f`
       const { data } = await axios.get(url);
       console.log(data.Search);
@@ -29,25 +31,46 @@ function Home() {
 
     }
   }
-  console.log();
 
   useEffect(() => {
     doApi();
-  }, [param, year])
+  }, [param,year])
 
+
+
+  let years = new Date().getFullYear()
+  let arr = [];
+  for (let i = 0; i < 30; i++) {
+    arr[i] = years - i;
+  }
+
+  
 
 
 
   return (
     <div>
-      <input placeholder='Search' onKeyDown={(e) => {
-        if (e.key === 'Enter') {
-          nav('/' + inputRef.current.value)
-        }
-      }} ref={inputRef} type="text" />
-
+      {/* Input search */}
       <div>
+        <input placeholder='Search' onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            nav('/' + inputRef.current.value)
+          }
+        }} ref={inputRef} type="text" />
+      </div>
+      <div>
+        {/* Timeline */}
+        {arr.map((item) => {
+          return (
+            <button onClick={() => {
+              setYear(item)
+            }}>{item}</button>
 
+          )
+        })}
+      </div>
+      {/* data */}
+      <div>
         {data ? <div>
           {loading ? <p>Loading...</p> :
 
