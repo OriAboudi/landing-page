c6f0e5cd5e3fe4ba45be59f0feff05f00c5e78ea
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2023-01-05 22:13:14 +0200

    ok

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import { useRef } from "react";
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
+import {FaLock} from 'react-icons/fa'
+
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
+
 
 function TimeLine({ setYear, arr, arrSelect }) { // Get props from Home.jsx
   const ref_select = useRef();
 
+
+  const navigation = [
+    { name: 'Home', href: '/', current: true }
+
+    // {arr.map((item) => {
+    //   return (
+    //     <button
+    //       className="border-t-neutral-50  ml-8 sm:none hover:bg-red-600 rounded-[50px] p-2"
+    //       onClick={() => {
+    //         setYear(item);
+    //       }}
+    //     >
+    //       {item}
+    //     </button>
+    //   );
+    // })}
+  
+  ]
+  
   return (
     <div>
       <div className="flex justify-between">
@@ -61,6 +93,120 @@ function TimeLine({ setYear, arr, arrSelect }) { // Get props from Home.jsx
       </div>
 
       <hr className="mx-[34px] my-3" />
+
+
+
+      <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
+            <div className="relative flex h-16 items-center justify-between">
+      
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <p  className="flex items-center">
+                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white underline">Top Year:</span>
+              </p>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                {/* <div className="flex flex-shrink-0 items-center">
+                  <FaLock className='block h-8 w-auto lg:hidden" 'color='white' size={'30px'} />
+                  <FaLock className='hidden h-8 w-auto lg:block" 'color='white' size={'30px'} />
+           
+                </div> */}
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {arr.map((item) => (
+                      <button
+                            className="border-t-neutral-50  ml-8 sm:none hover:bg-red-600 rounded-[50px] p-2"
+                            onClick={() => {
+                              setYear(item);
+                            }}
+                          >
+                            {item}
+                          </button>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* <button
+                  type="button"
+                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button> */}
+
+                {/* Profile dropdown */}
+                {/* <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                   
+                  </Transition>
+                </Menu> */}
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  to={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+
+
+
+
+
+
+
+
+
+
+
+
 
     </div>
   )
