24470ab2d16dd6242fcc17e04ec8ab7d29c990f7
Ori Aboudi <116178279+OriAboudi@users.noreply.github.com>
2023-01-05 21:50:20 +0200

    ok

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -16,7 +16,6 @@ function TimeLine({ setYear, arr, arrSelect }) { // Get props from Home.jsx
               </p>
 
               <button data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded="false">
-
                 <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
               </button>
 
